f3593df5c07fe815d781718b2758514cbdd8cc4e
akkarawit0 <119285508+RyuAkkarawit@users.noreply.github.com>
2024-09-15 20:16:37 +0700

    u

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'uer client'
 
 export default function Home() {
-  return <div className={styles.page}>TODO</div>;
+  return <div >TODO</div>;
+  
 }
--- src/lib/mongodb.tsx
+++ src/lib/mongodb.tsx
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
-
+ 
 //เอามาจาก MongoDB ของตัวเอง
-const uri = "mongodb+srv://akkarawitp:<db_password>@cluster0.rqmqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 let cachedDb: mongoose.Connection | null = null;
 
 export async function connectToDatabase() {
   if (cachedDb) return cachedDb;
-  const opts = { dbName: "Todo-app"};
-  const conn = await mongoose.connect(uri, opts);
-  cachedDb = conn.connection;
-  return cachedDb;
+  if(uri){
+    const opts = { dbName: "Todoapp"};
+    const conn = await mongoose.connect(uri, opts);
+    cachedDb = conn.connection;
+    return cachedDb;
+  }
+  return null;
 } 
